e59b96c3cd67ca046b8c027823fd147df320846a
rmzse <rodrigo@therawdelicious.com>
2016-11-27 22:12:50 +0100

    add the yaml file as init for file in spec

--- spec/bookshelf_spec.rb
+++ spec/bookshelf_spec.rb
@@ -3,9 +3,9 @@ require './lib/bookshelf.rb'
 describe Bookshelf do
 
   describe 'has the collection of library books' do
-    let(:file) { './lib/data.yml' }
-    before { touch(file) }
-    
+    # let(:file) { File.new }
+    before { file = YAML.load_file('./lib/data.yml') }
+
     it ' in a file that exists' do
       expect(file).to be_an_existing_file
     end
