bb943c41f777c1f1f82256e0d91ba478a7aa757f
rmzse <rodrigo@therawdelicious.com>
2016-11-27 22:01:05 +0100

    add test to check if data file exists

--- spec/bookshelf_spec.rb
+++ spec/bookshelf_spec.rb
@@ -1,9 +1,15 @@
 require './lib/bookshelf.rb'
 
 describe Bookshelf do
-  describe 'has the collection of library books' do
-    it ' in a file' do
 
+  before do
+    let(:file) { './lib/data.yml' }
+    before { touch(file) }
+  end
+
+  describe 'has the collection of library books' do
+    it ' in a file that exists' do
+      expect(file).to be_an_existing_file
     end
   end
 
