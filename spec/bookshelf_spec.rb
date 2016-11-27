46e234e850f01c3948f6cfb0d543ac261917fac5
rmzse <rodrigo@therawdelicious.com>
2016-11-27 22:04:38 +0100

    move the file inits out of the expectation 'it' and into the group example 'describe'

--- spec/bookshelf_spec.rb
+++ spec/bookshelf_spec.rb
@@ -3,10 +3,10 @@ require './lib/bookshelf.rb'
 describe Bookshelf do
 
   describe 'has the collection of library books' do
-
+    let(:file) { './lib/data.yml' }
+    before { touch(file) }
+    
     it ' in a file that exists' do
-      let(:file) { './lib/data.yml' }
-      before { touch(file) }
       expect(file).to be_an_existing_file
     end
   end
