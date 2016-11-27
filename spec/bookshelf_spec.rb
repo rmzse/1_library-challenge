96e7d2a89522fe73da746c6f83f2586d3ddcce8c
rmzse <rodrigo@therawdelicious.com>
2016-11-27 22:03:27 +0100

    move the data file initializer in spec from 'before' to expectation itself. wait with the including it in the example group 'describe' until later if it's necessary

--- spec/bookshelf_spec.rb
+++ spec/bookshelf_spec.rb
@@ -2,13 +2,11 @@ require './lib/bookshelf.rb'
 
 describe Bookshelf do
 
-  before do
-    let(:file) { './lib/data.yml' }
-    before { touch(file) }
-  end
-
   describe 'has the collection of library books' do
+
     it ' in a file that exists' do
+      let(:file) { './lib/data.yml' }
+      before { touch(file) }
       expect(file).to be_an_existing_file
     end
   end
