82a91d39594dbf5942fbb181d846c0d0cea6c5ce
rmzse <rodrigo@therawdelicious.com>
2016-11-27 22:14:07 +0100

    let file represent an object of File class

--- spec/bookshelf_spec.rb
+++ spec/bookshelf_spec.rb
@@ -3,7 +3,7 @@ require './lib/bookshelf.rb'
 describe Bookshelf do
 
   describe 'has the collection of library books' do
-    # let(:file) { File.new }
+    let(:file) { File.new }
     before { file = YAML.load_file('./lib/data.yml') }
 
     it ' in a file that exists' do
