8c40673d0eb4a3965a8ccb8a1d9285eab32de2a9
rmzse <rodrigo@therawdelicious.com>
2016-11-27 22:20:26 +0100

    give the first file init a shot with implementation, it seems somehow easier to understand the err msg

--- spec/bookshelf_spec.rb
+++ spec/bookshelf_spec.rb
@@ -3,8 +3,8 @@ require './lib/bookshelf.rb'
 describe Bookshelf do
 
   describe 'has the collection of library books' do
-    let(:file) { File.new( YAML.load_file('./lib/data.yml') ) }
-    # before { file = YAML.load_file('./lib/data.yml') }
+    # let(:file) { File.new( YAML.load_file('./lib/data.yml') ) }
+    before { file = YAML.load_file('./lib/data.yml') }
 
     it ' in a file that exists' do
       expect(file).to be_an_existing_file
